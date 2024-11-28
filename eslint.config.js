ff942b780a70b37a88b3e3652393890e33bfec91
ephemeras <ephemeras@qq.com>
2024-11-28 15:53:01 +0800

    chore(ci): add `.changeset` to ignore files for autofix

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import globals from 'globals'
 
 export default tsEslint.config([
   {
-    ignores: ['**/dist', 'docs/.vitepress/cache/']
+    ignores: ['**/dist', 'docs/.vitepress/cache/', '.changeset/']
   },
   eslint.configs.recommended,
   tsEslint.configs.recommended,
