74f16fc0809015fe041a9e1b5135a689693f75ac
ephemeras <ephemeras@qq.com>
2024-11-28 15:30:32 +0800

    chore(release): release version

--- packages/vitepress/tsup.config.ts
+++ packages/vitepress/tsup.config.ts
@@ -5,7 +5,7 @@ export const tsup: Options = {
   format: ['cjs', 'esm'],
   dts: true,
   splitting: false,
-  clean: process.env.NODE_ENV !== 'production',
+  clean: true,
   shims: false,
   external: [/.*\.vue/, /.*\.css/, /.*\.less/],
   ignoreWatch: [
